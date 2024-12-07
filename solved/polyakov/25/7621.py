f1b1e4aa49a868bdec58161672178e587fd4a41f
Stepan Starovoitov <stepan@the-devs.com>
2024-12-08 00:38:54 +0300

    fix 7621

--- solved/polyakov/25/7621.py
+++ solved/polyakov/25/7621.py
@@ -5,20 +5,24 @@ results = []
 for number in range(800_000, 10_000_000):
     min_div = None
     max_div = None
-    for i in range(2, number // 2):
+
+    for i in range(2, int(number ** 0.5)+1):
         if number % i == 0:
             min_div = i
             break
-    if min_div is None or min_div == number // 2:
+
+    if min_div is None:
         max_div = None
         continue
-    for i in range(number // 2, 1, -1):
-        if number % i == 0:
-            max_div = i
-            break
+
+    max_div = number // min_div
+
     M = max_div + min_div
+
     if M % 10 == 4:
         results.append([number, M])
+
         if len(results) == 5:
             break
+
 print(results)
